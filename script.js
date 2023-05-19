da474b705af183d350883c3f18ec5b45ea72eb5b
sahil zore <zoresahil80@gmail.com>
2023-05-19 18:25:52 +0530

    icons hide on input created

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 let name = document.getElementById('name');
 let logo = document.getElementById('logo');
+let input = document.getElementById('input');
+let rupee = document.getElementById('rupee');
+let camera = document.getElementById('camera');
 
-name.addEventListener('click',()=>{
-    let username = prompt("enter user name")
-    name.innerText = username;
+name.addEventListener('click', () => {
+  let username = prompt("enter user name")
+  name.innerText = username;
 })
 
 function importData() {
-    let input = document.createElement('input');
-    input.type = 'file';
-    input.onchange = _ => {
-      // you can use this method to get file and perform respective operations
-              let files =   Array.from(input.files);
-              
-              logo.src = URL.createObjectURL(files[0]);
-              
-          };
-    input.click();
-    
-  }
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = _ => {
 
-logo.addEventListener('click',importData)
+    let files = Array.from(input.files);
+
+    logo.src = URL.createObjectURL(files[0]);
+
+  };
+  input.click();
+
+}
+
+logo.addEventListener('click', importData)
+
+input.addEventListener('focus',()=>{
+  rupee.style.display = "none"
+  camera.style.display = "none"
+})
+
+
+input.addEventListener('blur',()=>{
+  rupee.style.display = "block"
+  camera.style.display = "block"
+})
